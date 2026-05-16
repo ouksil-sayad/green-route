747502d6852a5e95e0fadfd6c80c77e73984623f
ouksil-sayad <ouksil.sayad@ensia.edu.dz>
2026-05-16 02:20:14 +0100

    fix frontend api url for vercel

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 console.log("API_BASE_URL", API_BASE_URL);
 
 export interface ApiRouteRequest {
